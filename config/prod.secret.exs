5e833760cb45f560bea7fc6cb5ead9dc9d3ae3f2
Eric Camalionte <eric.camalionte@locaweb.com.br>
2017-05-24 16:33:34 -0300

    Create a config mechanism to read envvars during packaging process

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,7 +9,8 @@ config :pocapi, Pocapi.Endpoint,
   # Configure your database
   config :pocapi, Pocapi.Repo,
     adapter: Ecto.Adapters.Postgres,
-    username: System.get_env("DATABASE_USERNAME"),
-    password: System.get_env("DATABASE_PASSWORD"),
-    database: "pocapi_prod",
+    username: System.get_env("POCAPI_DATABASE_USERNAME"),
+    password: System.get_env("POCAPI_DATABASE_PASSWORD"),
+    database: System.get_env("POCAPI_DATABASE_NAME"),
+    hostname: System.get_env("POCAPI_DATABASE_HOSTNAME"),
     size: 20 # The amount of database connections in the pool
